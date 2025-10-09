605971fd725b58fb77e31e9b375096a6da958a55
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-09 11:36:54 +0700

    P6_tugas1

---
+++ src/p6/tugas2_17.java
@@ -0,0 +1,18 @@
+package p6;
+
+import java.util.Scanner;
+
+public class tugas2_17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+
+        System.out.print("Masukan angka: ");
+        int angka17 = jovita.nextInt();
+
+        if (angka17 % 2 == 0) {
+            System.out.println("Angka genap");
+        } else {
+            System.out.println("Angka ganjil");
+        }
+    }
+}
