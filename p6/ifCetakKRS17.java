a490a46602d30fc6fa4e492a8d42cd18b9e094dc
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-09 09:50:48 +0700

    P6_Percobaan1

---
+++ p6/ifCetakKRS17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ifCetakKRS17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS ---");
+        System.out.println("Apakah UKT sudah lunas? (true / false)");
+        boolean uktLunas17 = jovita.nextBoolean();
+
+        if (uktLunas17) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+    } 
+}
