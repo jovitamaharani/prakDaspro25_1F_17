3fa2bb98bb62cc58e5d7b90ea4b87f750d54d001
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-13 10:45:32 +0700

    CM_Siakad_17

--- src/p6/tugas2_17.java
+++ src/p6/tugas2_17.java
@@ -6,11 +6,13 @@ public class tugas2_17 {
     public static void main(String[] args) {
         Scanner jovita = new Scanner(System.in);
 
-        System.out.print("Masukan angka: ");
+        System.out.print("Masukan angka\t: ");
         int angka17 = jovita.nextInt();
 
         if (angka17 % 2 == 0) {
-            System.out.println("Angka genap");
+            System.out.println("======================================");
+            System.out.println("\tAngka genap\t");
+            System.out.println("======================================");
         } else {
             System.out.println("Angka ganjil");
         } 
