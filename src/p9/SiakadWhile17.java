2338cf8e9e7adc693fa6a2981b395e9ba8640d0a
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-23 12:05:09 +0700

    p9_Percobaan2

---
+++ src/p9/SiakadWhile17.java
@@ -0,0 +1,41 @@
+package p9;
+
+import java.util.Scanner;
+
+public class SiakadWhile17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+        double nilai17;
+
+        System.out.print("Masukan jumlah mahasiswa: ");
+        int jml17 = jovita.nextInt();
+
+        int i = 0;
+        while (i < jml17) {
+            System.out.print("Masukan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai17 = jovita.nextDouble();
+
+            if (nilai17 < 0 || nilai17 > 100) {
+                System.out.println("Nilai tidak valid. Masukan nilai yang valid!");
+                continue;
+            }
+
+            if (nilai17 > 80 && nilai17 <=100) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");    
+            } else if (nilai17 > 73 && nilai17 <= 80) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+            } else if (nilai17 > 65 && nilai17 <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai17 > 60 && nilai17 <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+            } else if (nilai17 > 50 && nilai17 <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai17 > 39 && nilai17 <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+            i++;
+        }
+    }
+}
