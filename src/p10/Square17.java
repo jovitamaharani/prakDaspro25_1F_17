0bf093058b3aafe0735de4251ecea237a09b5f26
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-30 09:12:17 +0700

    p10_Percobaan2

---
+++ src/p10/Square17.java
@@ -0,0 +1,19 @@
+package p10;
+
+import java.util.Scanner;
+
+public class Square17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+        
+        System.out.print("Masukan nilai N = ");
+        int n17 = jovita.nextInt();
+
+        for (int iOuter17 = 1; iOuter17 <= n17; iOuter17++) {
+            for (int i = 1; i <= n17; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
