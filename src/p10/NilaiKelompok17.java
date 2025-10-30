c8eb44e482ca71eed65c1f052ca1fae7e00c8b64
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-30 10:49:31 +0700

    p10_Percobaan4

---
+++ src/p10/NilaiKelompok17.java
@@ -0,0 +1,34 @@
+package p10;
+
+import java.util.Scanner;
+
+public class NilaiKelompok17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+
+        int i17, j17, nilai17;
+        float totalNilai17, rataNilai17, rataTertinggi17 = 0, rata17;
+
+        i17 = 1;
+
+        while (i17 <= 6) {
+            System.out.println("Kelompok " + i17);
+
+            totalNilai17 = 0;
+            for (j17 = 1; j17 <= 5; j17++) {
+                System.out.print("Nilai dari Kelompok Penilai " + j17 + ": ");
+                nilai17 = jovita.nextInt();
+                totalNilai17 += nilai17;
+            }
+            rataNilai17 = totalNilai17 / 5;
+            System.out.println("Kelompok" + i17 + " nilai rata-rata = " + rataNilai17);
+            if (rataNilai17 > rataTertinggi17) {
+                rataTertinggi17 = rataNilai17;
+            } 
+            i17++;
+        }
+
+        System.out.println("Rata-rata tertinggi adalah: " + rataTertinggi17);
+
+    }
+}
