512214b532e6551450d435b44518348951d048db
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-30 10:07:25 +0700

    p10_Percobaan3_PertanyaanNo3

---
+++ src/p10/Triangle17.java
@@ -0,0 +1,23 @@
+package p10;
+
+import java.util.Scanner;
+
+public class Triangle17 {
+    public static void main(String[] args) {
+        Scanner jovita = new Scanner(System.in);
+        
+        System.out.print("Masukan nilai n = ");
+        int n17 = jovita.nextInt();
+        int i17 = 0;
+
+        while (i17 <= n17) {
+            int j17 = 0;
+            while (j17 < i17) {
+                System.out.print("*");
+                j17++;
+            }
+            System.out.println();
+            i17++;
+        }
+    }
+}
