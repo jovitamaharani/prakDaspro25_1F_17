cd06010431ba1722fb19ab87e038ad0d13be1e9e
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-31 20:26:16 +0700

    p10_tugas1

--- src/p10/tugas1_17.java
+++ src/p10/tugas1_17.java
@@ -2,20 +2,21 @@ package p10;
 
 public class tugas1_17 {
     public static void main(String[] args) {
-        int total17 = 0;
-        String tampil17 = "";
-
+        
         for (int i = 1; i <= 5; i++) {
-            
-            int kuadrat17 = i * i;
-            total17 += kuadrat17;
-            
-            if (i == 1) {
-                tampil17 = "" + kuadrat17;
-            } else {
-                tampil17 += " + " + kuadrat17;
-            }
+            int total17 = 0;
+            String tampil17 = "";
 
+            for (int j = 1; j <= i; j++) {
+                int kuadrat17 = j * j;
+                total17 += kuadrat17;
+                if (i == 1) {
+                    tampil17 = "" + kuadrat17;
+                } else {
+                    tampil17 += " + " + kuadrat17;
+                }
+                
+            }  
             System.out.println("n = " + i + " -> jumlah kuadrat = " + tampil17 + " = " + total17);
         }
     }
