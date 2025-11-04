908ac05158e93d02409a4f5aeaa9950a496c1044
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-04 23:26:48 +0700

    p10_tugas1

--- src/p10/tugas1_17.java
+++ src/p10/tugas1_17.java
@@ -5,12 +5,12 @@ public class tugas1_17 {
         
         for (int i = 1; i <= 5; i++) {
             int total17 = 0;
-            String tampil17 = "";
+            String tampil17 = ""    ;
 
             for (int j = 1; j <= i; j++) {
                 int kuadrat17 = j * j;
                 total17 += kuadrat17;
-                if (i == 1) {
+                if (j == 1) {
                     tampil17 = "" + kuadrat17;
                 } else {
                     tampil17 += " + " + kuadrat17;
