362521b99db1522e04163ea69120cf6bbc582794
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-06 09:04:30 +0700

    p11_Percobaan1_PertanyaanNo2

--- src/p11/ArrayBilangan17.java
+++ src/p11/ArrayBilangan17.java
@@ -2,12 +2,7 @@ package p11;
 
 public class ArrayBilangan17 {
     public static void main(String[] args) {
-        int[] bil17 = new int[4];
-
-        bil17[0] = 5.0;
-        bil17[1] = 12867;
-        bil17[2] = 7.5;
-        bil17[3] = 2000000;
+        double[] bil17 = {5, 12867, 7.5, 2000000};
 
         System.out.println(bil17[0]);
         System.out.println(bil17[1]);
