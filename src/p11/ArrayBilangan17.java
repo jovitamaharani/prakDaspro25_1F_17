a16ab32f12e346af6b2d29cfc1fecbec6c936789
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-06 08:43:25 +0700

    p11_Percobaan1

---
+++ src/p11/ArrayBilangan17.java
@@ -0,0 +1,17 @@
+package p11;
+
+public class ArrayBilangan17 {
+    public static void main(String[] args) {
+        int[] bil17 = new int[4];
+
+        bil17[0] = 5;
+        bil17[1] = 13;
+        bil17[2] = -7;
+        bil17[3] = 17;
+
+        System.out.println(bil17[0]);
+        System.out.println(bil17[1]);
+        System.out.println(bil17[2]);
+        System.out.println(bil17[3]);
+    }
+}
