06a17d998bc68a77d3a352b72a7241141c7a4f9c
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-11 20:45:34 +0700

    p11_Percobaan1_PertanyaanNo4

--- src/p11/ArrayBilangan17.java
+++ src/p11/ArrayBilangan17.java
@@ -4,7 +4,7 @@ public class ArrayBilangan17 {
     public static void main(String[] args) {
         double[] bil17 = {5, 12867, 7.5, 2000000};
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil17[i]);
         }
     }
