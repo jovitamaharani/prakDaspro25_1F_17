cfd54c01a6f7b1e3ca6ebd018399c5faef7ad36d
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-11 22:20:44 +0700

    p11_Perccobaan4_PertanyaanNo2

--- src/p11/SearchNilai17.java
+++ src/p11/SearchNilai17.java
@@ -1,10 +1,23 @@
 package p11;
 
+import java.util.Scanner;
+
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int[] arrNilai17 = {80, 85, 78, 96, 90, 82, 86};
-        int key17 = 90;
+        Scanner jovita = new Scanner(System.in);
         int hasil17 = 0;
+        
+        System.out.print("Masukan banyaknya nilai yang akan diinput: ");
+        int jmlNilai17 = jovita.nextInt();
+        int[] arrNilai17 = new int[jmlNilai17];
+
+        for (int i = 0; i < jmlNilai17; i++) {
+            System.out.print("Masukan nilai mahasiswa ke-" + (i+1) + " : ");
+            arrNilai17[i] = jovita.nextInt();
+        }
+
+        System.out.print("Masukan nilai yang ingin dicari: ");
+        int key17 = jovita.nextInt();
 
         for (int i = 0; i < arrNilai17.length; i++) {
             if (key17 == arrNilai17[i]) {
@@ -14,7 +27,7 @@ public class SearchNilai17 {
         }
 
         System.out.println();
-        System.out.println("Nilai " + key17 + " ketemu di indeks ke-" + hasil17);
+        System.out.println("Nilai " + key17 + " ketemu, merupakan nilai mahasiswa ke-" + hasil17);
         System.out.println();   
     }
 }
