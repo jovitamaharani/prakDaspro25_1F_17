0503bbf92df93d883092fec0992be2536880a2db
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-11 21:24:53 +0700

    p11_Percobaan2_PertanyaanNo4

--- src/p11/ArrayNilai17.java
+++ src/p11/ArrayNilai17.java
@@ -7,13 +7,17 @@ public class ArrayNilai17 {
         Scanner jovita = new Scanner(System.in);
 
         int nilaiAkhir17[] = new int[10];
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukan nilai akhir ke-" + i + " : ");
+        for (int i = 0; i < nilaiAkhir17.length; i++) {
+            System.out.print("Masukan nilai akhir ke-" + (i + 1) + " : ");
             nilaiAkhir17[i] = jovita.nextInt();
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + (i + 1) + " adalah " + nilaiAkhir17[i]);
+            if (nilaiAkhir17[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus");
+            }
         }
     }
 }
