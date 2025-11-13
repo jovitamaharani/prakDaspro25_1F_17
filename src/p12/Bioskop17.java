f6f932094409af9ed001e606df9b85edca5ea757
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-13 12:18:09 +0700

    p12_Percobaan1_PertanyaanNo8

--- src/p12/Bioskop17.java
+++ src/p12/Bioskop17.java
@@ -19,8 +19,8 @@ public class Bioskop17 {
         System.out.printf("%s \t %s\n", penonton17[3][0], penonton17[3][1]);
 
         System.out.println("Penonton pada baris ke-3");
-        for (int i = 0; i < penonton17[2].length; i++) {
-            System.out.println(penonton17[2][i]);
+        for (String i : penonton17[2]) {
+            System.out.println(i);
         }
     }
 }
