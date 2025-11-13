b56c7602be53b8451cbb905ec78dae756ace2732
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-13 11:09:12 +0700

    p12_Percobaan1

---
+++ src/p12/Bioskop17.java
@@ -0,0 +1,20 @@
+package p12;
+
+public class Bioskop17 {
+    public static void main(String[] args) {
+        String[][] penonton17 = new String[4][2];
+
+        penonton17[0][0] = "Amin";
+        penonton17[0][1] = "Bena";
+        penonton17[1][0] = "Candra";
+        penonton17[1][1] = "Dela";
+        penonton17[2][0] = "Eka";
+        penonton17[2][1] = "Farhan";
+        penonton17[3][0] = "Gisel";
+
+        System.out.printf("%s \t %s\n", penonton17[0][0], penonton17[0][1]);
+        System.out.printf("%s \t %s\n", penonton17[1][0], penonton17[1][1]);
+        System.out.printf("%s \t %s\n", penonton17[2][0], penonton17[2][1]);
+        System.out.printf("%s \t %s\n", penonton17[3][0], penonton17[3][1]);
+    }
+}
