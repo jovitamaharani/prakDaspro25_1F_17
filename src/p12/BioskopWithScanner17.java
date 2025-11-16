15c899585d5e99b9485a8bbaf56ac0a4cbe32388
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-16 23:15:39 +0700

    p12_Percobaan2_PertanyaanNo2

--- src/p12/BioskopWithScanner17.java
+++ src/p12/BioskopWithScanner17.java
@@ -10,24 +10,45 @@ public class BioskopWithScanner17 {
         String nama17;
         String next17;
         int baris17;
-        int kolom17;
+        int kolom17, menu17;
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama17 = jovita.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris17 = jovita.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom17 = jovita.nextInt();
+            System.out.println("\n--- MENU BIOSKOP ---");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1-3): ");
+            menu17 = jovita.nextInt();
             jovita.nextLine();
 
-            penonton17[baris17 - 1][kolom17 - 1] = nama17;
+            switch (menu17) {
+                case 1:
+                    while (true) {
+                        System.out.print("Masukkan nama: ");
+                        nama17 = jovita.nextLine();
+                        System.out.print("Masukkan baris: ");
+                        baris17 = jovita.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom17 = jovita.nextInt();
+                        jovita.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next17 = jovita.nextLine();
+                        penonton17[baris17 - 1][kolom17 - 1] = nama17;
 
-            if (next17.equalsIgnoreCase("n")) {
-                break;
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next17 = jovita.nextLine();
+
+                        if (next17.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Menu tidak valid.");
+                    break;
             }
         }
     }
