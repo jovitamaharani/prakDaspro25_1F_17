ab3281ba2cad5154689f48ab8027db9ee3ad2ff2
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-20 01:00:10 +0700

    p12_Percobaan3

---
+++ src/p12/Numbers17.java
@@ -0,0 +1,10 @@
+package p12;
+
+public class Numbers17 {
+    public static void main(String[] args) {
+        int[][] myNumbers17 = new int[3][];
+        myNumbers17[0] = new int[5];
+        myNumbers17[1] = new int[3];
+        myNumbers17[2] = new int[1];
+    }
+}
