9a6bfae6c61edf5f4d989f6e0b661b0f6d49d843
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-20 01:00:35 +0700

    p12_Percobaan3_PertanyaanNo1

--- src/p12/Numbers17.java
+++ src/p12/Numbers17.java
@@ -1,10 +1,16 @@
 package p12;
 
+import java.util.Arrays;
+
 public class Numbers17 {
     public static void main(String[] args) {
         int[][] myNumbers17 = new int[3][];
         myNumbers17[0] = new int[5];
         myNumbers17[1] = new int[3];
         myNumbers17[2] = new int[1];
+
+        for (int i17 = 0; i17 < myNumbers17.length; i17++) {
+            System.out.println(Arrays.toString(myNumbers17[i17]));
+        }
     }
 }
