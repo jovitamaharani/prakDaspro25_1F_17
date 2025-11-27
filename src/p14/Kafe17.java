1393bdca49cd6646f9b11d04419236c5e6434c31
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-27 15:02:39 +0700

    p14_Percobaan1_PertanyaanNo2

--- src/p14/Kafe17.java
+++ src/p14/Kafe17.java
@@ -1,6 +1,6 @@
 public class Kafe17 {
-    public static void Menu17() {
-        System.out.println("===== MENU RESTO KAFE =====");
+    public static void main(String[] args) {
+         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam   Rp 15,000");
         System.out.println("2. Cappuccino   Rp 20,000");
         System.out.println("3. Latte        Rp 22,000");
@@ -8,9 +8,5 @@ public class Kafe17 {
         System.out.println("5. Roti Bakar   Rp 10,000");
         System.out.println("6. Mie Goreng   Rp 18,000");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-
-    public static void main(String[] args) {
-        Menu17();
     }
 }
